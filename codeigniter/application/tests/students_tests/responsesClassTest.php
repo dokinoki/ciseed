fcd6bd44a9f41598034b5d51010b5f88b711dee4
Guillermo <gbarba@deltatext.com>
2015-07-24 16:06:43 -0700

    - Finished backend tests (fixing git push error)

--- codeigniter/application/tests/students_tests/responsesClassTest.php
+++ codeigniter/application/tests/students_tests/responsesClassTest.php
@@ -1,4 +1,19 @@
 <?php
-    class responsesClassTest extends CIUnit_Framework_TestCase{
 
+    /**
+     * Class responsesClassTest
+     *
+     *  Test the libraries/responses_class.php file
+     */
+    class responsesClassTest extends CIUnit_Framework_TestCase{
+        /**
+         * Test POST reception
+         */
+        public function testGetRequest(){
+            $arrRequest = array('param1' => 1);
+            $_POST = $arrRequest;
+            $arrResponse = responses_class::getRequest();
+
+            $this->assertEquals($arrResponse, $arrResponse);
+        }
     }
